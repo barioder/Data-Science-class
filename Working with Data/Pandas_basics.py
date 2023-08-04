8e1665ca72bea0220493b65b447d20557140c95e
barioder <barioder27@gmail.com>
2023-08-04 23:23:14 +0300

    create time series sample data

--- Working with Data/Pandas_basics.py
+++ Working with Data/Pandas_basics.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 
 # Series
 # Series is like a list or 1D-array, but with index. All operations are index-aligned.
@@ -14,3 +16,25 @@ print("------B-----")
 print(b)
 print("------C-----")
 print(c)
+
+
+# timeseries
+#  In time series, index has a special structure - typically a range of dates or datetimes
+# Example to create time serie data 
+
+start_date = "Jan 1, 2023"
+end_date = "May 27, 2023"
+# getting the range of dates
+index = pd.date_range(start_date, end_date)
+
+print(index)
+
+# Creating sample data of a series 
+print("Threads Sample Data Created")
+threads_sold = pd.Series(np.random.randint(25,50, size=len(index)), index=index)
+print(threads_sold)
+
+threads_sold.plot(figsize=(10,3))
+plt.xlabel("Year 2023")
+plt.ylabel("Threads Sold")
+plt.show()
